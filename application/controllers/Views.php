a9696c892466145277de824d1bee4a69196a29fc
Wei Zhang <ink.wj.zhang@gmail.com>
2018-02-14 11:35:44 -0800

    Updated Views

---
+++ application/controllers/Views.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Views extends Application
+{
+
+    public function index()
+    {
+        $this->data['pagetitle'] = 'Ordered TODO List';
+        $tasks = $this->tasks->all();   // get all the tasks
+        $this->data['content'] = 'Ok'; // so we don't need pagebody
+        $this->data['leftside'] = 'by_priority';
+        $this->data['rightside'] = 'by_category';
+
+        $this->render('template_secondary');
+    }
+
+}
---
+++ application/views/by_category.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wei
+ * Date: 2/14/2018
+ * Time: 11:25 AM
+ */?>
+<h3>Tasks by Category</h3>
