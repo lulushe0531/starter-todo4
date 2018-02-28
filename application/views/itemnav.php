bef9810097e9be0e2c70ea022e6ecc8574da845e
lulushe0531 <shelu428@gmail.com>
2018-02-28 11:16:39 -0800

    finished job6 pagination, closes #14

---
+++ application/views/itemnav.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lulu
+ * Date: 2018-02-28
+ * Time: 11:11 AM
+ */
+?>
+
+<ul class="nav nav-pills">
+        <li><a href="/mtce/page/{first}">First</a></li>
+        <li><a href="/mtce/page/{previous}">Previous</a></li>
+        <li><a href="/mtce/page/{next}">Next</a></li>
+        <li><a href="/mtce/page/{last}">Last</a></li>
+</ul>
