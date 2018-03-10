447882c9aefbdb550854d6f104f16234e7b73b12
lulushe0531 <shelu428@gmail.com>
2018-03-10 00:01:50 -0800

    Added Task Entity Class, added tests folder, closes #29

---
+++ application/models/Task.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lulu
+ * Date: 2018-03-09
+ * Time: 11:00 PM
+ */
+
+class Task extends Entity
+{
+    protected $id;
+    protected $task;
+    protected $priority;
+    protected $size;
+    protected $group;
+    protected $deadline;
+    protected $status;
+    protected $flag;
+
+    // insist that an ID be present
+    public function setId($value) {
+        $this->id = $value;
+        return $this;
+    }
+
+    // insist that a task be present and no longer than 30 characters
+    public function setTask($value) {
+        $this->task = $value;
+        return $this;
+    }
+
+    public function setPriority($value) {
+        $this->priority = $value;
+        return $this;
+    }
+
+    public function setSize($value) {
+        $this->size = $value;
+        return $this;
+    }
+
+    public function setGroup($value) {
+        $this->group = $value;
+        return $this;
+    }
+
+    public function setDeadline($value) {
+        $this->deadline = $value;
+        return $this;
+    }
+
+    public function setStatus($value) {
+        $this->status = $value;
+        return $this;
+    }
+
+    public function setFlag($value) {
+        $this->flag = $value;
+        return $this;
+    }
+
+}
