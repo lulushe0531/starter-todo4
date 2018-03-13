59cd9b5fe1dd3d58188a20ab5b50c35d24700a55
Wei Zhang <ink.wj.zhang@gmail.com>
2018-03-13 14:13:15 -0700

    Form validation unit tests

--- tests/application/models/TaskTest.php
+++ tests/application/models/TaskTest.php
@@ -8,30 +8,66 @@ class TaskTest extends TestCase {
         $this->CI->load->model('task');
         $this->mTask = new Task();
         $this->mTask->setTask("Coding");
-        $this->mTask->setPriority("high");
-        $this->mTask->setGroup("small");
-        $this->mTask->setStatus("in progress");
+        $this->mTask->setPriority(2);
+        $this->mTask->setGroup(1);
+        $this->mTask->setSize(1);
     }
 
-//    function testValid() {
-//        $this->badTask = new Task();
-//        $this->expectException(Exception::class);
-//        $this->badTask->setTask("An extremely long name which is very lenghty for no reason expect to trigger this less than 64 characters limit we are trying to impose on each new task");
-//    }
+    function testValidTask() {
+        $this->badTask = new Task();
+        $this->expectException(Exception::class);
+        $this->badTask->setTask("An extremely long name which is very lenghty for no reason expect to trigger this less than 64 characters limit we are trying to impose on each new task");
+    }
+
+    function testValidPriorityType() {
+        $this->badTask = new Task();
+        $this->expectException(Exception::class);
+        $this->badTask->setPriority("Huge");
+    }
+
+    function testValidPrioritySize() {
+        $this->badTask = new Task();
+        $this->expectException(Exception::class);
+        $this->badTask->setPriority(5);
+    }
+
+    function testValidSizeType() {
+        $this->badTask = new Task();
+        $this->expectException(Exception::class);
+        $this->badTask->setSize("A Million");
+    }
+
+    function testValidSizeSize() {
+        $this->badTask = new Task();
+        $this->expectException(Exception::class);
+        $this->badTask->setSize(5);
+    }
+
+    function testValidGroupType() {
+        $this->badTask = new Task();
+        $this->expectException(Exception::class);
+        $this->badTask->setGroup("A Million");
+    }
+
+    function testValidGroupSize() {
+        $this->badTask = new Task();
+        $this->expectException(Exception::class);
+        $this->badTask->setGroup(6);
+    }
 
     function testPriority() {
-        $expected = "high";
-        $this->mTask->priority = $expected;
+        $expected = 2;
         $this->assertEquals($expected, $this->mTask->getPriority());
     }
 
     function testGroup() {
-        $expected = "small";
+        $expected = 1;
         $this->assertEquals($expected, $this->mTask->getGroup());
     }
 
-    function testStatus() {
-        $expected = "in progress";
-        $this->assertEquals($expected, $this->mTask->getStatus());
+    function testSize() {
+        $expected = 1;
+        $this->assertEquals($expected, $this->mTask->getGroup());
     }
+
 }
